06c3c4d6b1bf8cf4ea8e2711ab417d89caf5886c
federicorega <federico.rega@edu.unife.it>
2024-05-25 12:48:34 +0200

    swag nav testo + bottone

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,7 +9,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-custom">
         <a class="navbar-brand" href="#">
-        <img src="{{url('/images/sus.png')}}" width="389" height="106" class="d-inline-block align-top" alt="Logo">
+        <img src="{{url('/images/logo.png')}}" width="200" height="50" class="d-inline-block align-top" alt="Logo">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -17,7 +17,7 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
+                <li class="nav-item">
                     <a class="nav-link" href="#">Home</a>
                 </li>
                 <li class="nav-item">
@@ -26,6 +26,10 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Informazioni utili</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Accedi/Registrati</a>
+                </li>
+    
             </ul>
         </div>
     </nav>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,15 @@
 <x-layout>
-    <h1>{{$greeting}} from the homepage</h1>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="Iscriviti">
+    <!-- Inserisci il tuo testo qui sopra il carousel -->
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <p>
+                    <span class="first-sentence">Sei stanco di dover chiamare ogni volta per prenotare al ristorante o dal parrucchiere?</span><br>
+                    Scegli la comodità e la praticità di prenotare online con il nostro servizio semplice e veloce!
+                </p>
+                <button class="btn btn-primary custom-button">Prenota ora</button>
+            </div>
+        </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Accedi">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Prenotati">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+    <br>
 </x-layout>
-
