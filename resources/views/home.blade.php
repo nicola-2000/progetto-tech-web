ae0b17cea32b3f8a4f1a4567047570b2d931e88f
federicorega <federico.rega@edu.unife.it>
2024-05-26 10:22:25 +0200

    sium

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,15 @@
 <x-layout>
+    <!-- Inserisci il tuo testo qui sopra il carousel -->
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <p>
+                    <br><span class="first-sentence">Sei stanco di dover chiamare ogni volta per prenotare al ristorante o dal parrucchiere?</span><br>
+                    Scegli la comodità e la praticità di prenotare online con il nostro servizio semplice e veloce!
+                </p>
+                <button class="btn btn-primary custom-button">Prenota ora</button>
+            </div>
+        </div>
     <h1>{{$greeting}} from the homepage</h1>
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
